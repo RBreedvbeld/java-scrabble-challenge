94c4479140b20292888038f7b157e3066ee4c26a
Rose Breedveld <rosebreedveld@hotmail.com>
2023-05-10 11:04:01 -0400

    Passed scrabble standards

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,5 +1,27 @@
 package com.booleanuk;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 
 public class Scrabble {
+       String word;
+       Scores letterScores;
 
+       public Scrabble(String word) {
+              this.letterScores = new Scores();
+              this.word = word.toUpperCase().trim();
+       }
+
+       public int score() {
+              int wordScore = 0;
+              for (int i = 0; i < this.word.length(); i++) {
+                     char ch = this.word.charAt(i);
+//                     wordScore += this.letterScores.scores.get(ch);
+                     wordScore += this.letterScores.getScore(ch);
+              }
+              // it returns the value of the wordScore
+              return wordScore;
+       }
 }
---
+++ src/main/java/com/booleanuk/notes.java
@@ -0,0 +1,13 @@
+package com.booleanuk;
+
+public class notes {
+    /*
+    - verschil hashmap, array en arraylist
+    - tekeningetje gemaakt
+    - je stopt er een woord in en en komt een score uit
+    woord waarde in opslaan. Dat doe je in een variabele die je een eigen naam geeft
+    - je moet alle letters tracken op de waarde. Met een forloop en met de lengte van de array
+    - haal er een enkele letter uit met charAt
+
+     */
+}
